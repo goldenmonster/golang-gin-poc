9b3878e052b607604de568de17288fbf7d8abd73
goldenmonster <goldenmonster@gmail.com>
2023-06-19 23:23:12 -0400

    added video service and controoler

--- server.go
+++ server.go
@@ -1,16 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/goldenmonster/golang-gin-poc/controller"
+	"github.com/goldenmonster/golang-gin-poc/service"
+)
+
+var (
+	videoService service.VideoService = service.New()
+	videoControoler controller.VideoController = controller.New(videoService)
+)
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/test", func (ctx *gin.Context)  {
-
-		ctx.JSON(200, gin.H{
-			"message": "OK",
-		})		
+	server.GET("/videos", func (ctx *gin.Context)  {
+		ctx.JSON(200, videoControoler.FindAll) 
 	})
+
+	server.POST("/videos", func (ctx *gin.Context)  {
+		ctx.JSON(200, videoControoler.Save(ctx)) 
+	})
+	
 
 	server.Run(":8080")
 }
